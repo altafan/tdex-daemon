363df693c3cdbac06ba03ec0c7674be8d18dad70
altafan <pietro@vulpem.com>
2022-01-27 17:14:59 +0100

    Update portable RepoManager: * Drop tx  and tx methods * Add methods to register handlers for occuring trade/withdrawal events

--- internal/core/ports/db.go
+++ internal/core/ports/db.go
@@ -1,8 +1,6 @@
 package ports
 
 import (
-	"context"
-
 	"github.com/tdex-network/tdex-daemon/internal/core/domain"
 )
 
@@ -14,29 +12,12 @@ type RepoManager interface {
 	WithdrawalRepository() domain.WithdrawalRepository
 
 	Close()
-
-	NewTransaction() Transaction
-	NewPricesTransaction() Transaction
-	NewUnspentsTransaction() Transaction
-	RunTransaction(
-		ctx context.Context,
-		readOnly bool,
-		handler func(ctx context.Context) (interface{}, error),
-	) (interface{}, error)
-	RunUnspentsTransaction(
-		ctx context.Context,
-		readOnly bool,
-		handler func(ctx context.Context) (interface{}, error),
-	) (interface{}, error)
-	RunPricesTransaction(
-		ctx context.Context,
-		readOnly bool,
-		handler func(ctx context.Context) (interface{}, error),
-	) (interface{}, error)
-}
-
-// Transaction interface defines the method to commit or discard a database transaction.
-type Transaction interface {
-	Commit() error
-	Discard()
+	RegisterHandlerForTradeEvent(
+		eventType domain.TradeEventType,
+		handler func(event domain.TradeEvent),
+	)
+	RegisterHandlerForWithdrawalEvent(
+		eventType domain.WithdrawalEventType,
+		handler func(event domain.WithdrawalEvent),
+	)
 }
