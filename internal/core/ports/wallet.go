97f2b31ee4e15260e2444a8e14530592e78864f3
altafan <pietro@vulpem.com>
2022-01-27 17:04:25 +0100

    Update ocean portable interface

--- internal/core/ports/wallet.go
+++ internal/core/ports/wallet.go
@@ -2,7 +2,7 @@ package ports
 
 import "context"
 
-type Wallet interface {
+type OceanWallet interface {
 	WalletManager() WalletManager
 	AccountManager() AccountManager
 	TransactionManager() TransactionManager
@@ -10,45 +10,78 @@ type Wallet interface {
 }
 
 type WalletManager interface {
-	GenSeed(ctx context.Context) (mnemonic string, err error)
-	CreateWallet(ctx context.Context, mnemonic, password string) error
-	RestoreWallet(ctx context.Context, mnemonic, password string) error
-	Unlock(ctx context.Context, password string) error
-	ChangePassword(ctx context.Context, oldPassword, newPassword string) error
-	Status(ctx context.Context) (code uint64, desc string, err error)
+	GenSeed(ctx context.Context) (mnemonic []string, err error)
+	CreateWallet(
+		ctx context.Context, mnemonic []string, passphrase string,
+		chMessages chan string,
+	) error
+	RestoreWallet(
+		ctx context.Context, mnemonic []string, passphrase string,
+		chMessages chan string,
+	) error
+	Unlock(ctx context.Context, passphrase string) error
+	ChangePassword(
+		ctx context.Context, currentPassphrase, newPassphrase string,
+	) error
+	Status(ctx context.Context) (status WalletStatus, err error)
 	GetInfo(
 		ctx context.Context,
-	) (rootPath, masterBlindingKey string, accounts []WalletAccount, err error)
+	) (info WalletInfo, err error)
 }
 
 type AccountManager interface {
 	CreateAccount(
 		ctx context.Context, name string,
-	) (index uint64, xpub string, err error)
+	) (accountIndex uint64, xpub string, err error)
 	DeriveAddressesForAccount(
-		ctx context.Context, accountIndex, numOfAddresses uint64,
+		ctx context.Context, account string, numOfAddresses uint64,
+	) ([]AddressInfo, error)
+	DeriveChangeAddressForAccount(
+		ctx context.Context, account string, numOfAddresses uint64,
 	) ([]AddressInfo, error)
 	ListAddressesForAccount(
-		ctx context.Context, accountIndex uint64,
+		ctx context.Context, account string,
 	) ([]AddressInfo, error)
 	BalanceForAccount(
-		ctx context.Context, accountIndex uint64,
+		ctx context.Context, account string,
 	) (balancePerAsset map[string]Balance, err error)
-	DeleteAccount(ctx context.Context, accountIndex uint64) error
+	DeleteAccount(ctx context.Context, account string) error
+	ListUtxosForAccount(
+		ctx context.Context, account string,
+	) (spendableUtxos, lockedUtxos []Utxo, err error)
 }
 
 type TransactionManager interface {
 	SelectUnspentsForAccount(
-		ctx context.Context, accountIndex uint64,
+		ctx context.Context, account string,
 		targetAsset string, targetAmount uint64, strategy Strategy,
-	)
+	) (utxos []UtxoKey, change uint64, err error)
+	EstimateFees(
+		ctx context.Context, inputs []Input, outputs []Output,
+	) (feeAmount uint64, err error)
 	TransferFromAccount(
-		ctx context.Context, accountIndex uint64, receivers []Receiver,
+		ctx context.Context, account string, outputs []Output,
+		millisatPerByte uint64,
 	) (txHex string, err error)
+	CreateTransaction(
+		ctx context.Context, inputs []Input, outputs []Output,
+	) (psetBase64 string, err error)
+	UpdateTransaction(
+		ctx context.Context, psetBase64 string, inputs []Input, outputs []Output,
+	) (
+		updatedPset string,
+		inBlindKeysByScript, outBlindKeysByScrpt map[string][]byte,
+		err error,
+	)
+	BlindTransaction(
+		ctx context.Context, psetBase64 string, lastBlinder bool,
+	) (blindedPsetBase64 string, err error)
 	SignTransaction(
-		ctx context.Context, psetBase64 string,
+		ctx context.Context, psetBase64 string, extractRawTx bool,
 	) (signedPsetBase64 string, err error)
-	BroadcastTransaction(txHex string) (txid string, err error)
+	BroadcastTransaction(
+		ctx context.Context, txHex string,
+	) (txid string, err error)
 }
 
 type NotificationManager interface {
@@ -56,12 +89,37 @@ type NotificationManager interface {
 	UtxoChannel() chan UtxoNotification
 }
 
+type WalletInfo interface {
+	Network() Network
+	NativeAsset() string
+	RootPath() string
+	MasterBlindingKey() string
+	Accounts() []WalletAccount
+}
+
 type WalletAccount interface {
 	Index() uint64
-	LastInternalIndex() uint64
-	LastExternalIndex() uint64
+	Name() string
 	DerivationPath() string
 	Xpub() string
+}
+
+type Network interface {
+	IsUnknown() bool
+	IsMainnet() bool
+	IsTestnet() bool
+	IsRegtest() bool
+}
+
+type WalletStatus interface {
+	IsInitialized() bool
+	IsSynced() bool
+	IsUnlocked() bool
+}
+
+type InitWalletMsg interface {
+	Error() error
+	Message() string
 }
 
 type AddressInfo interface {
@@ -85,14 +143,24 @@ type TxNotification interface {
 
 type UtxoNotification interface {
 	AccountIndex() uint64
-	Utxo() Utxo
+	Utxo() UtxoKey
+	EventType() UtxoEventType
+	BlockDetails() BlockDetails
+	TxDetails() TxDetails
 }
 
 type TxEventType interface {
-	IsUknown() bool
+	IsUnknown() bool
 	IsTxBroadcasted() bool
 	IsTxUnconfirmed() bool
 	IsTxConfirmed() bool
+}
+
+type UtxoEventType interface {
+	IsUnknown() bool
+	IsUtxoLocked() bool
+	IsUtxoUnlocked() bool
+	IsUtxoSpent() bool
 }
 
 type BlockDetails interface {
@@ -101,34 +169,34 @@ type BlockDetails interface {
 	Timestamp() int64
 }
 
+type TxDetails interface {
+	Hash() string
+	Hex() string
+}
+
 type Strategy interface {
-	IsUknown() bool
+	IsUnknown() bool
 	IsBranchBound() bool
 	IsFragment() bool
 }
 
-type Utxo interface {
+type UtxoKey interface {
 	TxID() string
 	Index() uint32
-	Asset() string
-	Value() uint64
-	AssetCommitment() string
-	ValueCommitment() string
-	AssetBlinder() []byte
-	ValueBlinder() []byte
-	Script() []byte
-	Nonce() []byte
-	RangeProof() []byte
-	SurjectionProof() []byte
-	IsConfidential() bool
-	IsConfirmed() bool
-	IsRevealed() bool
-	IsNew() bool
-	IsSpent() bool
 }
 
-type Receiver interface {
-	Address() string
+type Utxo interface {
+	Key() UtxoKey
 	Asset() string
-	Amount() uint64
+	Value() uint64
+	Script() []byte
+	IsConfirmed() bool
+	IsLocked() bool
+}
+
+type Input = UtxoKey
+type Output interface {
+	Asset() string
+	Value() uint64
+	Address() string
 }
